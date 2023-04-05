4fb7cec5addf3c37d99703ac731cb13a8e92d77e
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-05 20:52:45 +0300

    Project init and initial setup

---
+++ crm/accounts/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home page')
+
+def products(request):
+    return HttpResponse('Products page')
+
+def customer(request):
+    return HttpResponse('Customer page')
