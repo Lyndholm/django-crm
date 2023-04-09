332533bc937e7a4122e7b1cdf4e5feba71feb83f
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-09 13:29:10 +0300

    Models and migrations

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=64, null=True)
+    phone = models.CharField(max_length=64, null=True)
+    email = models.CharField(max_length=64, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+    )
+
+    name = models.CharField(max_length=128, null=True)
+    description = models.CharField(max_length=1024, null=True)
+    category = models.CharField(max_length=64, null=True, choices=CATEGORY)
+    price = models.FloatField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    # customer = ...
+    # product = ...
+    status = models.CharField(max_length=64, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
